2d2a511c675de9918f6e3bd964ec0955052bf6d8
maharjan-umesh <spangelrocks@gmail.com>
2025-01-14 17:39:56 -0600

    google ad integration

--- components/googlead/HorizontalBanner.tsx
+++ components/googlead/HorizontalBanner.tsx
@@ -1,10 +1,25 @@
+import { useEffect } from 'react';
+
 export default function HorizontalBanner() {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.adsbygoogle) {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+  }, []);
+
   return (
     <div className='container mx-auto p-4 my-2'>
-      <div className='bg-blue-500 text-white p-8 rounded-lg'>
+      <div className='p-8 rounded-lg'>
         <div className='flex items-center justify-between'>
           <div>
-            <h2 className='text-lg font-bold'>Goog Ad code here!</h2>
+            <ins
+              className='adsbygoogle'
+              style={{ display: 'block' }}
+              data-ad-client='ca-pub-9770704114610149'
+              data-ad-slot='5657566101'
+              data-ad-format='auto'
+              data-full-width-responsive='true'
+            ></ins>
           </div>
         </div>
       </div>
